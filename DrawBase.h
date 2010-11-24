c0e47f3baf51819bdecd59ea0b84291e848ae4ad
pandolf <>
2010-11-24 12:04:16 +0000

    This commit was manufactured by cvs2svn to create tag 'fp2010-11-24-bis'.
    Sprout from master 2010-11-21 16:19:35 UTC pandolf 'higher pt bins'
    Cherrypick from master 2010-11-24 12:04:15 UTC pandolf 'now supporting multiple data histograms':
        DrawBase.C
        DrawBase.h

--- DrawBase.h
+++ DrawBase.h
@@ -26,9 +26,11 @@
 struct InputFile {
   TFile* file;
   std::string datasetName;
+  float weight;
   std::string legendName;
   int fillColor;
   int fillStyle;
+  int markerStyle;
 };
 
 
@@ -60,21 +62,24 @@ class DrawBase {
   void set_sameEventNormalization();
   void set_sameInstanceNormalization();
 
-  void drawHisto( const std::string& name, const std::string& etaRegion, const std::string& flags, const std::string& axisName="", int legendQuadrant=1, bool log_aussi=false);
+  //void drawHisto( const std::string& name, const std::string& etaRegion, const std::string& flags, const std::string& axisName="", const std::string& units="", int legendQuadrant=1, bool log_aussi=false);
+  void drawHisto( const std::string& name, const std::string& axisName, const std::string& units="", const std::string& instanceName="Entries", bool log_aussi=false, int legendQuadrant=1, const std::string& labelText="", const std::string& flags="" );
   void drawProfile( const std::string& yVar, const std::string& xVar, int legendQuadrant=1);
   void drawStack(const std::string& varY, const std::string& varX, const std::string& RECO_GEN, bool isData) const { this->drawStack( varY, varX, "", RECO_GEN, isData); };
   void drawStack(const std::string& varY, const std::string& varX, const std::string& etaRegion, const std::string& RECO_GEN, bool isData) const;
-  void compareDifferentHistos( const std::vector< HistoAndName > histosandnames, const std::string xAxisName, const std::string saveVarName, bool normalized=true, int legendQuadrant=1 );
-  void compareDifferentHistos_singleFile( InputFile file, const std::vector< HistoAndName > histosandnames, const std::string xAxisName, const std::string saveVarName, bool normalized=true, int legendQuadrant=1 );
+  void compareDifferentHistos( const std::vector< HistoAndName > histosandnames, const std::string saveVarName, const std::string xAxisName, const std::string& units="", const std::string& instanceName="Entries", bool normalized=true, int legendQuadrant=1 );
+  void compareDifferentHistos_singleFile( InputFile file, const std::vector< HistoAndName > histosandnames, const std::string saveVarName, const std::string xAxisName, const std::string& units="", const std::string& instanceName="Entries", bool normalized=true, int legendQuadrant=1 );
   void drawObjects( const std::vector< TObject* > objects, const std::string& name, 
                    const std::string& xAxisName, float xMin, float xMax, 
                    const std::string& yAxisName, float yMin, float yMax, 
                    bool logx=false, bool logy=false);
 
   void set_analysisType( const std::string analysisType ) { analysisType_ = analysisType; };
-  void add_dataFile( TFile* dataFile, const std::string& datasetName );
-  void add_mcFile( TFile* mcFile, const std::string& datasetName, const std::string& legendName, int fillColor, int fillStyle=-1 );
-  void set_outputdir( const std::string& outputdir="" ); //if "" is passed, default outputdir is set (recommended)
+  void add_dataFile( TFile* dataFile, const std::string& datasetName, const std::string& legendName="DATA", int markerColor=-1, int markerStyle=-1 );
+  void add_mcFile( TFile* mcFile, const std::string& datasetName, const std::string& legendName, int fillColor=-1, int fillStyle=-1 );
+  // in the following function weight must be cross_section(in pb) / Nevents:
+  void add_mcFile( TFile* mcFile, float weight, const std::string& datasetName, const std::string& legendName, int fillColor=-1, int fillStyle=-1 );
+  void set_outputdir( const std::string& outputdir="" ); //if "" is passed, default outputdir is set
   void set_flags( const std::string& flags ) { flags_ = flags; };
   void set_pt_thresh( Int_t pt_thresh ) { pt_thresh_ = pt_thresh; };
   void set_etamax( Float_t etamax ) { etamax_ = etamax; };
@@ -92,7 +97,7 @@ class DrawBase {
   std::string get_analysisType() const { return analysisType_; };
   std::string get_recoType() const { return recoType_; };
   std::string get_flags() const { return flags_; };
-  TFile* get_dataFile() const { return dataFile_.file; };
+  TFile* get_dataFile( int i ) const { return dataFiles_[i].file; };
   TFile* get_mcFile( int i ) const { return mcFiles_[i].file; };
   std::string get_outputdir() const { return outputdir_; };
   Int_t get_pt_thresh() const { return pt_thresh_; };
@@ -118,7 +123,7 @@ class DrawBase {
 
   std::string flags_;
 
-  InputFile dataFile_;
+  std::vector< InputFile > dataFiles_;
   std::vector< InputFile > mcFiles_;
   
   Float_t scaleFactor_;
