1b74b54ea0e9d789cb7a9d3de3d4ea5e57858f12
pandolf <>
2012-03-06 12:21:37 +0000

    first commit

---
+++ TreeFinalizer.C
@@ -0,0 +1,135 @@
+#include "TreeFinalizer.h"
+#include <iostream>
+#include "TROOT.h"
+#include <cstdlib>
+
+
+
+TreeFinalizer::TreeFinalizer( const std::string& analyzerType, const std::string& dataset, const std::string& flags, const std::string& recoType, const std::string& jetAlgo ) {
+
+  DEBUG_ = false;
+
+
+  tree_ = new TChain("jetTree");
+
+  analyzerType_ = analyzerType;
+  inputAnalyzerType_ = analyzerType;
+  dataset_ = dataset;
+  flags_ = flags;
+
+  recoType_ = recoType;
+  jetAlgo_ = jetAlgo;
+
+  outFile_ = 0;
+
+  nCounter_ = 0.;
+  nCounterW_ = 0.;
+  nCounterPU_ = 0.;
+
+} //constructor
+
+
+
+TreeFinalizer::~TreeFinalizer() {
+
+  std::cout << std::endl << "-> Histograms saved in: " << outFile_->GetName() << std::endl;
+
+  if( tree_!=0 ) {
+    delete tree_;
+    tree_=0;
+  }
+
+} //destructor
+
+
+
+void TreeFinalizer::createOutputFile( const std::string& additionalFlags ) {
+
+   std::string outfileName;
+
+   if( DEBUG_ ) outfileName = "prova_"+dataset_;
+   else {
+    if(dataset_!="") outfileName = analyzerType_ + "_" + dataset_;
+    else outfileName = analyzerType_;
+   }
+
+
+   if( flags_!="" )
+     outfileName = outfileName + "_" + flags_;
+   if( additionalFlags!="" )
+     outfileName = outfileName + "_" + additionalFlags;
+
+   outfileName = outfileName + "_" + this->getJetType();
+   outfileName = outfileName + ".root";
+
+   outFile_ = TFile::Open(outfileName.c_str(), "RECREATE");
+   
+   outFile_->cd();
+
+
+}
+
+
+
+
+
+void TreeFinalizer::addInput( const std::string& dataset ) {
+
+    std::string infileName = analyzerType_ + "_2ndLevelTreeW_" + dataset + "_" + this->getJetType();
+    infileName += ".root";
+    TFile* infile = TFile::Open(infileName.c_str(), "read");
+    if( infile==0 ) {
+      std::cout << "Didn't find file '" << infileName << "'. Did you forget to finalize (i.e. the \"W\")?" << std::endl;
+      std::cout << "Exiting." << std::endl;
+      exit(77);
+    }
+    std::string treeName = infileName +"/jetTree";
+    tree_->Add(treeName.c_str());
+    std::cout << "-> Added " << treeName << ". Tree has " << tree_->GetEntries() << " entries." << std::endl;
+    infile->Close();
+
+
+} //addinput
+
+
+
+
+
+std::vector<TH1F*> TreeFinalizer::getResponseHistos(const std::string& name, unsigned binArraySize, Double_t* ptBins) {
+
+  std::vector<TH1F*> returnVector;
+
+  for( unsigned i=0; i<(binArraySize-1); ++i ) {
+    char histoName[100];
+    sprintf( histoName, "%s_ptBin_%.0f_%.0f", name.c_str(), ptBins[i], ptBins[i+1]);
+    int nbins = 50;
+    float xmin = 0.5;
+    float xmax = 2.;
+    TH1F* newHisto = new TH1F(histoName, "", nbins, xmin, xmax);
+    newHisto->Sumw2();
+    returnVector.push_back(newHisto);
+  }
+
+  return returnVector;
+
+}
+
+
+void TreeFinalizer::writeResponseHistos( TFile* file, std::vector<TH1F*> h1_response, std::string dirName ) {
+
+  file->mkdir( dirName.c_str() );
+  file->cd( dirName.c_str() );
+
+  for( unsigned iHisto=0; iHisto<h1_response.size(); ++iHisto ) h1_response[iHisto]->Write();
+
+  file->cd();
+
+}
+
+
+
+std::string TreeFinalizer::getJetType() const {
+
+  return (recoType_ + jetAlgo_);
+
+}
---
+++ TreeFinalizer.h
@@ -0,0 +1,69 @@
+// ------------------------------------------------------------
+//  
+//    TreeFinalizer - Base class for finalizing the analysis
+//    Used in the Ntp1 tree workflow, after a Ntp1Analyzer
+//    class has produced 2nd level trees 
+//    (and merge_and_setWeights has done its job)
+//
+//    It is an abstract class: the method 'finalize' has to be
+//    implemented.
+//
+// ------------------------------------------------------------
+
+
+#include <vector>
+#include "TChain.h"
+#include "TH1F.h"
+#include "TFile.h"
+
+
+
+class TreeFinalizer {
+
+
+ public:
+
+  TreeFinalizer( const std::string& analyzerType, const std::string& dataset, const std::string& flags="", const std::string& recoType="pf", const std::string& jetAlgo="akt5" );
+  virtual ~TreeFinalizer();
+
+  void createOutputFile( const std::string& additionalFlags="" );
+  virtual void addInput(const std::string& dataset);
+
+  std::vector<TH1F*> getResponseHistos(const std::string& name, unsigned binArraySize, Double_t* ptBins);
+  void writeResponseHistos( TFile* file, std::vector<TH1F*> h1_response, std::string dirName );
+
+  TChain* get_tree() { return tree_; };
+  TFile* get_outFile() { return outFile_; };
+  bool get_DEBUG() { return DEBUG_; };
+
+  std::string getJetType() const;
+
+  void set_outFile( const std::string& fileName="", const std::string& suffix="" );
+  void set_inputAnalyzerType( const std::string& analyzerType ) { inputAnalyzerType_ = analyzerType; };
+  void set_flags( const std::string& flags ) { flags_ = flags; };
+  void set_DEBUG( bool DEBUG ) { DEBUG_ = DEBUG; };
+
+  virtual void finalize() = 0;
+
+
+  TChain* tree_;
+
+  float nCounter_;
+  float nCounterW_;
+  float nCounterPU_;
+
+  std::string analyzerType_;
+  std::string inputAnalyzerType_;
+  std::string dataset_;
+  std::string flags_;
+
+  std::string recoType_;
+  std::string jetAlgo_;
+
+  TFile* outFile_;
+
+  bool DEBUG_;
+
+ private:
+
+};
